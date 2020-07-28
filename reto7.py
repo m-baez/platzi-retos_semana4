b0071b5dfccd89b93a614c2b6070bad55d4470e9
Marco Baez <mbr.personal@hotmail.com>
2020-07-28 11:47:51 -0500

    add challenge 7

--- reto7.py
+++ reto7.py
@@ -7,16 +7,34 @@ Tu usuario debe poder elegir alguna de estas figuras, indicar la distancia de su
 
 
 def options():
-    print('-', '- '*17)
-    print("|    Elige una opción disponible   |")
-    print("|    1. Triángulo equilatero       |")
-    print("|    2. Triángulo isoseles         |")
-    print("|    3. Tríangulo escaleno         |")
-    print("|    4. Cuadrado                   |")
-    print("|    5. Pentágono                  |")
-    print("|                                  |")
-    print("|    0. Salir                      |")
-    print(' -'*17, '-')
+    print(' -'*48)
+    print("|    Calcula el perímetro y área de una de las siguientes figuras, elige una opción disponible   |".upper())
+    print("|                                                                                                |")
+    print("|    1.- Triángulo equilatero                                                                    |")
+    print("|    2.- Triángulo isósceles                                                                     |")
+    print("|    3.- Cuadrado                                                                                |")
+    print("|    4.- Pentágono                                                                               |")
+    print("|                                                                                                |")
+    print("|    0.- Salir                                                                                   |")
+    print(' -'*48)
+
+
+def menu():
+    print(' ='*22, '\n')
+    print('    Elige una de las opciones disponibles   \n'.upper())
+    print('    1.- Calcular de nuevo con valores distintos')
+    print('    2.- Elegir otra figura')
+    print('    0.- Salir')
+    print('\n', ' ='*22)
+
+    userInput = int(input("Seleccione una opción: "))
+
+    if userInput == 0:
+        print("\n¡Gracias por usar nuestra calculadora!")
+    elif userInput == 1:
+        equilateralTriangle()
+    elif userInput == 2:
+        main()
 
 
 def equilateralTriangle():
@@ -24,32 +42,63 @@ def equilateralTriangle():
     calcSquare = math.pow(side, 2) - math.pow(side/2, 2)
     heigth = math.sqrt(calcSquare)
     triangleArea = (side * heigth) / 2
-    print('\nEste es el perimetro: %.2fcm' % (side*3))
-    print('Y esta es el área: %.2fcm' % triangleArea)
+    print('\nEste es el perímetro: %.2fcm' % (side*3))
+    print('Y esta es el área: %.2fcm\n' % triangleArea)
+    menu()
 
 
-def triangle():
-    pass
+def isoscelesTriangle():
+    sides = float(input('¿Cúanto miden los lados tu triángulo en cm? '))
+    base = float(input('¿Y cúanto mide la base? '))
+    calcSquare = math.pow(sides, 2) - math.pow(base/2, 2)
+    heigth = math.sqrt(calcSquare)
+    area = base * heigth / 2
+    print('\nEste es el perímetro: %.2fcm' % ((sides*2)+base))
+    print('Y esta es el área: %.2fcm\n' % area)
+    menu()
 
 
-def menu():
+def square():
+    sides = float(input('¿Cúanto miden los lados de tu cuadrado? '))
+    print('\nEste es el perímetro: %.2fcm' % (sides*4))
+    print('Y esta es el área: %.2fcm\n' % math.pow(sides, 2))
+
+
+def pentagon():
+    sides = float(input('¿Cúanto miden los lados de tu pentágono? '))
+    tangent = math.tan(math.radians(36)) * 2
+    apothem = sides / tangent
+    area = (5 * sides * apothem) / 2
+    print('\nEste es el perímetro: %.2fcm' % (sides*5))
+    print('Y esta es el área: %.2fcm\n' % area)
+
+
+def main():
     while True:
         options()
         try:
-            userInput = int(input("Seleccione una opcion: "))
-            if userInput in range(6):
+            userInput = int(input("Seleccione una opción: "))
+            if userInput in range(5):
                 if userInput == 0:
-                    print("Adios! Vuelva pronto")
+                    print("\n¡Gracias por usar nuestra calculadora!")
                     break
                 elif userInput == 1:
                     equilateralTriangle()
                     break
-                print("\nHas elegido la opción %d\n" % userInput)
+                elif userInput == 2:
+                    isoscelesTriangle()
+                    break
+                elif userInput == 3:
+                    square()
+                    break
+                elif userInput == 4:
+                    pentagon()
+                    break
             else:
-                print('Error, solo de aceptan numeros del 0 al 5')
+                print('\nError, solo de aceptan numeros del 0 al 5')
         except ValueError:
-            print("Error, ingrese solamente numeros")
+            print("\nError, ingrese solamente numeros")
 
 
 if __name__ == '__main__':
-    menu()
+    main()
